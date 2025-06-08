aa4fb3fd600da3aae134fd55093a43a64e4c95cc
CodeB1ack <patrickcledford@gmail.com>
2025-06-08 10:28:05 -0400

    added registration form

--- Project4/src/Components/Assets/LoginRegister/LoginRegister.jsx
+++ Project4/src/Components/Assets/LoginRegister/LoginRegister.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LoginRegister.css";
-import { FaUser, FaLock  } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope  } from "react-icons/fa";
 
 const LoginRegister = () => {
     return (
@@ -25,7 +25,35 @@ const LoginRegister = () => {
                     </div>
                     <button type="submit" className="btn">Login</button>
                     <div className="register-link"> 
-                        <p>Don't have an account?</p> <a href="#">Register</a>
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                </form>
+            </div>
+
+            <div className="form-box register">
+                <form action="">
+                    <h1>Registration</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required/>
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="email" placeholder="Email" required/>
+                        <FaEnvelope className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required/>
+                        <FaLock className="icon"/>
+                    </div>
+                    <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox" />
+                            I agree to the Terms & Conditions
+                        </label>
+                    </div>
+                    <button type="submit" className="btn">Register</button>
+                    <div className="register-link"> 
+                        <p>Already have an account? <a href="#">Login</a></p>
                     </div>
                 </form>
             </div>
