cf2ba8a40b35d9635e86e01511ed079d10d5d7ab
CodeB1ack <patrickcledford@gmail.com>
2025-06-10 13:54:57 -0400

    setup routes

--- Project4/src/App.jsx
+++ Project4/src/App.jsx
@@ -1,15 +1,27 @@
 
 import LoginRegister from './Components/Assets/LoginRegister/LoginRegister'
+import Header from './Components/Assets/Header'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    
-      <div>
-        <LoginRegister />
-      </div>
-     
+    <Routes>
+      <Route index element={  
+        <div>
+      <main>
+        <Header />
+      </main> 
+    </div> 
+  } />
+      <Route path={'./login'} element={
+          <div>
+          <LoginRegister />
+        </div>
+      }/>
+    </Routes>
   )
+  
 }
 
 export default App
---
+++ Project4/src/Components/Assets/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="" className="logo">Main</a>
+            <nav className="nav">
+              <a href="">Login</a>
+              <a href="">Register</a>
+            </nav>
+        </header>
+    );
+}
